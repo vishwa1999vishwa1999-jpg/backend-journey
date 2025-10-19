428ece4ae54c6c50ffb1ea1b533aded02bd9e9c6
Bhuvanes Waran <bhuvaneswaran@Bhuvaness-MacBook-Air.local>
2025-10-19 18:10:25 +0530

    Day 2: Add loops,milestone tracker,and counter

---
+++ counter.py
@@ -0,0 +1,10 @@
+for i in range(1,11):
+    print(f"Day {i} of learning")
+
+total = 0
+for day in range(7):
+    total = total + 4
+    print(f"day{day+1} you stuied {4} hours(cumulative: {total})")
+print(f"\nTotal hours this week: {total}")      
+
+
---
+++ milestone_tracker.py
@@ -0,0 +1,12 @@
+for day in range(1,31):
+    print(f"Day {day}: Keep going!")
+    if day == 7 :
+        print(f"\n🎉 One week complete!")
+    elif day == 14 :
+        print(f"\n🔥 Two week complete!")
+    elif day == 21 :
+        print(f"\n💪 Two week complete!")
+    elif day == 30 :
+        print(f"\n🏆 One month complete! You're unstoppable!")            
+else:
+    print(" ")
